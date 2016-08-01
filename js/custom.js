5969ed725d04e2041b054b59b8700b4e24ef081b
Laura Machado <lauramachado1021@gmail.com>
2016-08-01 10:37:37 -0400

    Add navbar

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,37 @@
-var aboutMe = false;
-var resume = false;
-var contact = false;
+getNavBar = function(){
+  document.write("<nav class=\"navbar navbar-inverse navbar-fixed-top\" role=\"navigation\">");
+document.write("    <div class=\"container\">");
+document.write("        <!-- Brand and toggle get grouped for better mobile display -->");
+document.write("        <div class=\"navbar-header\">");
+document.write("            <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\">");
+document.write("                <span class=\"sr-only\">Toggle navigation<\/span>");
+document.write("                <span class=\"icon-bar\"><\/span>");
+document.write("                <span class=\"icon-bar\"><\/span>");
+document.write("                <span class=\"icon-bar\"><\/span>");
+document.write("            <\/button>");
+document.write("        <\/div>");
+document.write("        <!-- Collect the nav links, forms, and other content for toggling -->");
+document.write("        <div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">");
+document.write("            <ul class=\"nav navbar-nav\">");
+document.write("              <li>");
+document.write("                <a class=\"navbar-brand \" href=\"index.html\">Laura Machado<\/a>");
+document.write("              <\/li>");
+document.write("                <li>");
+document.write("                    <a href=\"aboutme.html\">About Me<\/a>");
+document.write("                <\/li>");
+document.write("                <li>");
+document.write("                    <a href=\"resume.html\">Resume<\/a>");
+document.write("                <\/li>");
+document.write("                <li>");
+document.write("                  <a href=\"contact.html\">Contact<\/a>");
+document.write("                <\/li>");
+document.write("");
+document.write("            <\/ul>");
+document.write("        <\/div>");
+document.write("        <!-- \/.navbar-collapse -->");
+document.write("    <\/div>");
+document.write("    <!-- \/.container -->");
+document.write("<\/nav>");
+document.write("");
+
+}
